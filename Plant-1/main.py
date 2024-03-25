f652ac5b942cd80506888bd1ca9be31ef6579874
toiQS <88361704+toiQS@users.noreply.github.com>
2024-03-25 15:14:07 +0700

    update

--- Plant-1/main.py
+++ Plant-1/main.py
@@ -1,7 +1,25 @@
-from flask import Flask, request, jsonify
+# use flask to run the app and create a api localhost with http://127.0.0.1:5000
+
+from flask import Flask,request,jsonify
 app = Flask(__name__)
 
+# create a route for get_users
 
+
+# @app.route("/get_users/<user_id>")
+# def get_users(user_id):
+#     user_data = {
+#         "user_id": user_id,
+#         "name": "Akai",
+#     }
+#     extra = request.args.get("extra")
+#     if extra:
+#         user_data["extra"] = extra
+#     return jsonify(user_data),200
+
+
+
+# create a route for home
 @app.route("/")
 def home():
     return "Home"
@@ -9,3 +27,6 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
