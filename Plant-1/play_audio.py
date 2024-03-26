495d6f8b39dfbb0a8fd2ca64b0f7b8e02e86cfcf
toiQS <88361704+toiQS@users.noreply.github.com>
2024-03-26 19:09:21 +0700

    Update play_audio.py

--- Plant-1/play_audio.py
+++ Plant-1/play_audio.py
@@ -5,7 +5,7 @@
 from dotenv import load_dotenv
 import os
 import base64
-from requests import post
+from requests import post,get   
 import json
 
 load_dotenv()
@@ -33,5 +33,33 @@ def get_token():
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
 
+
+def search(token, artust_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"q={artust_name}&type=artist&limit=1"
+    query_url = url + "?" + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+
+    if len(json_result) == 0:
+        print("No artist found with that name")
+        return None
+    return json_result[0]
+def get_song_by_artist(token, artist_id):
+    url = "https://api.spotify.com/v1/artists/" + artist_id + "/top-tracks"
+    headers = get_auth_header(token)
+    query = "market=US"
+    query_url = url + "?" + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+
 token = get_token()
-print(token)    
+search_for_artist = search(token, "kendrick lamar")
+artist_id = search_for_artist["id"]
+# print(search_for_artist["name"])
+songs = get_song_by_artist(token, artist_id)
+for index, songs in enumerate(songs):
+    print(index + 1, songs["name"])
